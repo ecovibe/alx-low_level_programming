ef8d74eefb3913e086d6a68536e9e3f4c5c41b69
ecovibe <talktoengineermali@gmail.com>
2023-07-18 22:15:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,34 @@
+#include "main.h"
+
+
 /**
-* print_alphabet_x10 - Print lowercase alphabet 10 times
-*/
+
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
+
+ */
+
+
 void print_alphabet_x10(void)
+
 {
-char num, letter;
-for (num = 0; num <= 9; num++)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar(letter);
-}
-_putchar('\n');
-}
+
+        char num, letter;
+
+
+        for (num = 0; num <= 9; num++)
+
+        {
+
+                for (letter = 'a'; letter <= 'z'; letter++)
+
+                {
+
+                        _putchar(letter);
+
+                }
+
+                _putchar('\n');
+
+        }
+
 }
