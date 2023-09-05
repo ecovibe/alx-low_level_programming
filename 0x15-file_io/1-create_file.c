a559b2b7e2ddc8434f738ae8bb2601f26761f210
ecovibe <talktoengineermali.gmail.com>
2023-09-04 18:15:02 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,33 @@
-#include <stdlib.h>
-#include "function_pointers.h"
+#include "main.h"
 
 /**
- * array_iterator - executes a function given as a parameter
- * on each element of an array
- * @array: array to iterate over
- * @size: size of the array
- * @action: pointer to function used
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+int create_file(const char *filename, char *text_content)
 {
-	unsigned int i;
+	int o, w, len = 0;
 
-	if (!array || !action)
-		return;
+	if (filename == NULL)
+		return (-1);
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
